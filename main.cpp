3d82b14416bb463102fee2af82cfa07306ff305a
Alex <orlov.aleksei@phystech.edu>
2020-11-02 14:29:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,11 +8,40 @@ using std::deque;
 
 typedef long long ll;
 
+void type_plus(deque<ll>& left, deque<ll>& right){
+    ll num;
+    cin >> num;
+    right.push_back(num);
+    if (left.size() < right.size()) {
+        left.push_back(right.front());
+        right.pop_front();
+    }
+}
+
+void type_multiply(deque<ll>& left, deque<ll>& right){
+    ll num;
+    cin >> num;
+    if (left.size() > right.size()) {
+        right.push_front(num);
+    }
+    else {
+        left.push_back(num);
+    }
+}
+
+void type_subtract(deque<ll>& left, deque<ll>& right){
+    cout << left.front() << '\n';
+    left.pop_front();
+    if (left.size() < right.size()) {
+        left.push_back(right.front());
+        right.pop_front();
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
     ll n;
     cin >> n;
     char type;
@@ -20,31 +49,13 @@ int main() {
     for (ll i = 0; i < n; ++i) {
         cin >> type;
         if (type == '+') {
-            ll num;
-            cin >> num;
-            right.push_back(num);
-            if (left.size() < right.size()) {
-                left.push_back(right.front());
-                right.pop_front();
-            }
+            type_plus(left, right);
         }
         else if (type == '*') {
-            ll num;
-            cin >> num;
-            if (left.size() > right.size()) {
-                right.push_front(num);
-            }
-            else {
-                left.push_back(num);
-            }
+            type_multiply(left, right);
         }
         else {
-            cout << left.front() << '\n';
-            left.pop_front();
-            if (left.size() < right.size()) {
-                left.push_back(right.front());
-                right.pop_front();
-            }
+            type_subtract(left, right);
         }
     }
 }
