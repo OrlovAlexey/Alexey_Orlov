c884ba600486d23320ca5f6caf90c6c2815c6c2e
Alex <orlov.aleksei@phystech.edu>
2021-03-30 23:33:52 +0300

    Исправил

--- main.cpp
+++ main.cpp
@@ -11,29 +11,27 @@ using std::max;
 
 typedef long long ll;
 
-void dp_solution(int n, const vector<ll>& v) {
+void dp_solution(int n, const vector<ll>& input) {
     const ll inf = 10000000000;
-    vector<ll> p(n+1);
-    vector<ll> dp(n+1);
-    vector<ll> prev_p(n);
-    p[0] = -1;
-    dp[0] = -inf;
+    vector<ll> parents(n + 1);
+    vector<ll> dynamic_programming(n + 1);
+    vector<ll> previous_parents(n);
+    parents[0] = -1;
+    dynamic_programming[0] = -inf;
     for (ll i = 1; i <= n; ++i) {
-        dp[i] = inf;
+        dynamic_programming[i] = inf;
     }
     ll length = 0;
     for (ll i = 0; i < n; ++i) {
-        ll j = (ll)(std::upper_bound(dp.begin(), dp.end(), v[i]) - dp.begin());
-        if (dp[j-1] <= v[i] && v[i] <= dp[j]) { // зачем это проверять, j вроде выбирается так, чтобы это выполнялось
-            dp[j] = v[i];
-            p[j] = i; // однобуквенные имена переменных лучше не использовать
-            prev_p[i] = p[j-1];
-            if (length < j)
-                length = j;
-        }
+        ll j = (ll)(std::upper_bound(dynamic_programming.begin(), dynamic_programming.end(), input[i]) - dynamic_programming.begin());
+        dynamic_programming[j] = input[i];// убрал условие, поменял переменные
+        parents[j] = i;
+        previous_parents[i] = parents[j-1];
+        if (length < j)
+            length = j;
     }//filling in dp[i], parents[i], previous_parents[i]
     cout << length << '\n';
-    for (ll j = p[length]; j > -1; j = prev_p[j]) {
+    for (ll j = parents[length]; j > -1; j = previous_parents[j]) {
         cout << n - j << " ";
     }//searching and printing answer
 }
@@ -44,13 +42,13 @@ int main() {
     cout.tie(0);
     ll n;
     cin >> n;
-    vector<ll> original(n), v(n);
+    vector<ll> original(n), reversed(n);
     for (ll i = 0; i < n; ++i) {
         cin >> original[i];
     }
     for (ll i = 0; i < n; ++i) {
-        v[i] = original[n-1-i];
+        reversed[i] = original[n-1-i];
     }
-    dp_solution(n, v);
+    dp_solution(n, reversed);
     return 0;
 }
