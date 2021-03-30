eecd9dd86b20870bd60ef3a4989e382cd0833311
Alex <orlov.aleksei@phystech.edu>
2021-03-31 00:00:24 +0300

    еще исправил

--- main.cpp
+++ main.cpp
@@ -6,30 +6,38 @@ using std::cout;
 using std::vector;
 using std::max;
 
-int main() { // все в main
-    int S, N;
-    cin >> S >> N;
-    vector<int> w(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> w[i];
-    }
-    vector<vector<int>> dp(S+1);
-    for (int i = 0; i < S+1; ++i) {
-        dp[i].resize(N+1);
-        for (int j = 0; j < N+1; ++j) {
+void filling_base(int capacity, int number_of_gold, vector<vector<int>>& dp) {
+    for (int i = 0; i < capacity + 1; ++i) {
+        dp[i].resize(number_of_gold + 1);
+        for (int j = 0; j < number_of_gold + 1; ++j) {
             dp[i][j] = 0;
         }
     }
-    for (int i = 1; i < N+1; ++i) {
-        for (int j = 1; j < S+1; ++j) {
-            if (w[i-1] <= j) {
-                dp[j][i] = max(dp[j][i-1], dp[j - w[i-1]][i-1] + w[i-1]);
+}
+
+void solution_actually(int capacity, int number_of_gold, const vector<int>& weights, vector<vector<int>>& dp) {
+    for (int i = 1; i < number_of_gold + 1; ++i) {
+        for (int j = 1; j < capacity + 1; ++j) {
+            if (weights[i-1] <= j) {
+                dp[j][i] = max(dp[j][i-1], dp[j - weights[i-1]][i-1] + weights[i-1]);
             }
             else {
                 dp[j][i] = dp[j][i-1];
             }
         }
     }
-    cout << dp[S][N] << '\n';
+    cout << dp[capacity][number_of_gold] << '\n';
+}
+
+int main() {
+    int capacity, number_of_gold;
+    cin >> capacity >> number_of_gold;
+    vector<int> weights(number_of_gold);
+    for (int i = 0; i < number_of_gold; ++i) {
+        cin >> weights[i];
+    }
+    vector<vector<int>> dp(capacity + 1);
+    filling_base(capacity, number_of_gold, dp);
+    solution_actually(capacity, number_of_gold, weights, dp);
     return 0;
 }
