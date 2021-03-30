443330976d74c838fde5e4e98434b3670eca71c5
Alex <orlov.aleksei@phystech.edu>
2021-03-30 23:52:11 +0300

    Исправил

--- main.cpp
+++ main.cpp
@@ -4,38 +4,40 @@
 using std::cin;
 using std::cout;
 using std::vector;
+using std::max;
 
-int main() { // все в main, однобуквенные имена переменных
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n), b(m);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+long long solution(long long n, const vector<long long>& first_sequence, long long m, const vector<long long>& second_sequence, vector<vector<long long>>& dp) {
+    for (long long i = 0; i <= n; ++i) {
+        for (long long j = 0; j <= m; ++j) {
+            dp[i][j] = 0;
+        }
     }
-    for (int j = 0; j < m; ++j) {
-        cin >> b[j];
-    }
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-    vector<int> p(m+1);
-    int best;
-    for (int i = 1; i <= n; ++i) {
-        best = 0;
-        for (int j = 1; j <= m; ++j) {
-            dp[i][j] = dp[i - 1][j];
-            if (a[i-1] == b[j-1] && dp[i-1][j] < best + 1) {
-                dp[i][j] = best + 1;
+    for (long long i = 1; i <= n; ++i) {
+        for (long long j = 1; j <= m; ++j) {
+            if (first_sequence[i-1] == second_sequence[j-1]) {
+                dp[i][j] = max(1 + dp[i-1][j-1], max(dp[i-1][j], dp[i][j-1]));
             }
-            if (a[i-1] > b[j-1] && dp[i-1][j] > best) {
-                best = dp[i-1][j];
+            else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
     }
-    int ans = 0;
-    for (int j = 1; j <= m; ++j) {
-        if (ans < dp[n][j]) {
-            ans = dp[n][j];
-        }
+    return dp[n][m];
+}
+
+int main() {
+    long long n, m;
+    cin >> n; // length of first_sequence
+    vector<long long> first_sequence(n);
+    for (long long i = 0; i < n; ++i) {
+        cin >> first_sequence[i];
     }
-    cout << ans << '\n';
+    cin >> m; // length of second_sequence
+    vector<long long> second_sequence(m);
+    for (long long j = 0; j < m; ++j) {
+        cin >> second_sequence[j];
+    }
+    vector<vector<long long>> dp(n+1, vector<long long>(m+1));
+    cout << solution(n, first_sequence, m, second_sequence, dp) << '\n';
     return 0;
 }
